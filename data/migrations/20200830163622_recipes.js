d489ed4dae4e46c3e680abd26fd749294233460c
Vish Singh <vishalicious213@gmail.com>
2020-08-30 16:42:26 -0700

    added up/down functions for recipes migration

--- data/migrations/20200830163622_recipes.js
+++ data/migrations/20200830163622_recipes.js
@@ -1,8 +1,16 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipes', tbl => {
+        // create primary key called id
+        tbl.increments();
+        // name, description, completed
+        tbl.text('name').notNullable();
+        tbl.integer('ingredients');
+        tbl.integer('instructions');
+    })
 };
 
 exports.down = function(knex) {
-  
+    // drop the whole table
+    return knex.schema.dropTableIfExists('recipes');
 };
