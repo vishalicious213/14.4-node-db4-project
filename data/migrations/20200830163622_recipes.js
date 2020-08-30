b4ba22a9a327f073ea312e18682f52490e21ae90
Vish Singh <vishalicious213@gmail.com>
2020-08-30 16:49:06 -0700

    added up/down functions for ingredients migration

--- data/migrations/20200830163622_recipes.js
+++ data/migrations/20200830163622_recipes.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('recipes', tbl => {
         // create primary key called id
         tbl.increments();
-        // name, description, completed
+        // name, ingredients (to ingredients.id), instructions (to instructions.id)
         tbl.text('name').notNullable();
         tbl.integer('ingredients');
         tbl.integer('instructions');
